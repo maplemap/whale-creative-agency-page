51ff7e319ffbce1ed508265a69f96cdd0940ce6a
Sergey.Illarionov <easthunch@gmail.com>
2016-06-20 14:22:16 +0300

    update bundle

--- client/main.js
+++ client/main.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App, Page, Section, Hero, Navbar, HorizontalSplit } from "neal-react";
-import SamplePage from './sample-page.jsx';
-import '../node_modules/neal-react/css/base.scss';
 
-ReactDOM.render((
-    <App>
-        <SamplePage />
-    </App>
-), document.getElementById("main"));
+import './main.less';
+import App from './components/App.jsx';
+
+if (module.hot) {
+    module.hot.accept();
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("main")
+);
