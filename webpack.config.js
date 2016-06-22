745b31ec7a556a379133ae58e9c344cb3799ea8c
Sergey.Illarionov <easthunch@gmail.com>
2016-06-22 10:32:53 +0300

    add: fonts

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                 ]
             },
             {
-                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                test   : /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
                 loader : 'file?name=[name].[ext]'
             },
             {
