e6e2b20cba59096106e866f3dce45b9330ddc613
Sergey.Illarionov <easthunch@gmail.com>
2016-06-22 18:25:59 +0300

    add: slide 1

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import GuidLines from './GuidLines.jsx';
 import Slider from 'react-slick';
 
 class Presentation extends React.Component {
@@ -14,13 +15,32 @@ class Presentation extends React.Component {
         };
 
         return(
-            <section className="">
+            <section className="section-part presentation">
                 <Slider {...sliderSettings}>
-                    <div><h3>1</h3></div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
+                    <div>
+                        <div className="section-part__slides presentation__slide-1">
+                            <button className="presentation__break-btn">break it<br /> with us</button>
+                            <div className="presentation__intro">
+                                <h2 className="presentation__lead-in">imagination<br /> is the <span className="presentation__lead-in--underline">only</span> limit.</h2>
+                                <div className="presentation__text">
+                                    we are keeping our hand on pulse of trends<br />but still always one step ahead
+                                </div>
+                            </div>
+                            <span className="presentation__blur-block" />
+                            <svg xmlns="http://www.w3.org/2000/svg"  version="1.1">
+                                <defs>
+                                    <filter id="blur">
+                                        <feGaussianBlur stdDeviation="5"/>
+                                    </filter>
+                                </defs>
+                            </svg>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </Slider>
+                <GuidLines />
             </section>
 
         )
