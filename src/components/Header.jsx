12325d1b9b389e7eea7f081fa2fceddbf6d868f3
Sergey.Illarionov <easthunch@gmail.com>
2016-06-28 17:38:52 +0300

    add: team section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ class Header extends React.Component{
         return (
             <header className="header">
                 <div className="header__body">
-                    <a href="#" className="logo logo--header">
-                        Whale<span className="logo__star">*</span><br/> Creative<br/> Agency
-                    </a>
-                    <div className="team-position  team-position--header">full-stack<br /> design services</div>
+                    <h1>
+                        <a href="#" className="logo logo--header">
+                            Whale<span className="logo__star">*</span><br/> Creative<br/> Agency
+                        </a>
+                    </h1>
+                    <div className="section-name">full-stack<br /> design services</div>
                     <nav className="navigation" />
                 </div>
             </header>
