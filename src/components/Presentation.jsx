ed3c8dcaa88166551b5d047fc10cd65fae6057d8
Sergey.Illarionov <easthunch@gmail.com>
2016-06-28 13:45:46 +0300

    add: slide 4 of presentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import GuidLines from './GuidLines.jsx';
 import Slider from 'react-slick';
-import './Presenation.less';
+import './Presentation.less';
 
 
 class Presentation extends React.Component {
@@ -61,7 +61,23 @@ class Presentation extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="presentation__slide"></div>
+                    <div className="presentation__slide presentation__slide--4">
+                        <div className="presentation__body">
+                            <div className="presentation__intro">
+                                <h2 className="presentation__lead-in">
+                                    branding
+                                </h2>
+                                <div className="presentation__text">
+                                    we will make your  brand is unique<br /> and noticable
+                                </div>
+                            </div>
+                            <div className="terminal-42 terminal-42--presentation">
+                                <span className="terminal-42__color">#F8E71C</span><br />
+                                <span className="terminal-42__name">Terminal 42</span><br />
+                                <span className="terminal-42__text">coworking<br /> space</span>
+                            </div>
+                        </div>
+                    </div>
                 </Slider>
                 <button className="presentation__more-btn">More</button>
                 <GuidLines />
