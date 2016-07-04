71d3858cc12fa3281517698c652a827be2bbaae7
Sergey.Illarionov <easthunch@gmail.com>
2016-07-04 11:11:53 +0300

    fix: add classnames and disabled lines

--- src/components/GuidLines.jsx
+++ src/components/GuidLines.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 import './GuidLines.less';
 
+import classNames from 'classnames';
+
 class GuidLines extends React.Component {
     static defaultProps = {
         lines: [1, 2, 3, 4, 5, 6]
     };
 
     render() {
+        const disableLines = this.props.disableLines;
+
+        const guidClassConditions = {
+            "guid-lines--white": (this.props.colorScheme === 'white')
+        };
+        const guidClass = classNames("guid-lines", guidClassConditions);
 
         return (
-            <div className="guid-lines guid-lines--white">
+            <div className={ guidClass }>
                 { this.props.lines.map((num, index) => {
-                    const spanClass = 'guid-lines__' + num;
+                    const spanClassConditions = {
+                      hidden: ( disableLines && disableLines.indexOf(num) + 1 )
+                    };
+                    const spanClass = classNames(`guid-lines__${num}`, spanClassConditions);
 
-                    return <span key={index} className={spanClass} />
+                    return <span
+                                key={index}
+                                className={spanClass}
+                           />
                 })}
             </div>
         )
