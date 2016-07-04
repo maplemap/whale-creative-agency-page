a1ba51600eb028f79a6f02a9b1147f0e7d7b8b00
Sergey.Illarionov <easthunch@gmail.com>
2016-07-04 17:45:20 +0300

    add: Parallax component

--- src/utils/ParallaxBg.jsx
+++ src/utils/ParallaxBg.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 class ParallaxBackground extends React.Component {
+    static defaultProps = {
+        parallaxSpeed: 0.03
+    };
+
+    state = {
+        positionX: '',
+        positionY: '',
+        style: {}
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+
+        const parallaxBox = ReactDOM.findDOMNode( this.refs.parallaxBox );
+        const bgPosition = window.getComputedStyle(parallaxBox).getPropertyValue("background-position").split(' ');
+
+        this.setState({
+            positionX: parseInt(bgPosition[0], 10),
+            positionY: parseInt(bgPosition[1], 10)
+        });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        const newPositionY = this.state.positionY - window.pageYOffset * this.props.parallaxSpeed;
+
+        this.setState({
+            style: {
+                backgroundPosition: `${this.state.positionX}% ${newPositionY}%`
+            }
+        });
+    };
 
     render() {
         return(
-            <div className={this.props.className}>
+            <div
+                ref="parallaxBox"
+                className={this.props.className}
+                style={this.state.style}
+            >
                 {this.props.children}
             </div>
         )
