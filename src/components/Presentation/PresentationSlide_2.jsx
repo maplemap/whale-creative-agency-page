83dddae9c5ce19ea3c5b2fed77b4e04e05eca609
Sergey.Illarionov <easthunch@gmail.com>
2016-07-08 11:10:09 +0300

    add: fix background-clip for Firefox

--- src/components/Presentation/PresentationSlide_2.jsx
+++ src/components/Presentation/PresentationSlide_2.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Parallax from '../../utils/ParallaxBg.jsx';
 
-class Slide_2 extends React.Component {
 
+class Slide_2 extends React.Component {
     render() {
         return(
             <div className="presentation__slide presentation__slide--2">
                 <div className="presentation__body">
                     <h2 className="brand-name brand-name--presentation">
                         <Parallax
-                            parallaxSpeed={.15}
+                            parallaxSpeed={.1}
                             className="brand-name__text"
                         >
-                            <i className="brand-name__star">*</i>
+                            <sup className="brand-name__star">*</sup>
                             <i className="brand-name__W">w</i>
-                            <i className="brand-name__H">h</i>
-                            <i className="brand-name__A">a</i><br />
+                            <i className="brand-name__H">h</i><br />
+                            <i className="brand-name__A">a</i>
                             <i className="brand-name__L">l</i><br />
                             <i className="brand-name__E">e</i>
                         </Parallax>
