ca3dfa7fa889bf1e626f6ec27d19caccdb7019f8
Sergey.Illarionov <easthunch@gmail.com>
2016-07-08 17:27:02 +0300

    fix: wrong name

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -52,14 +52,14 @@ class Portfolio extends React.Component {
     };
 
     render() {
-        let GalleryReview = null;
+        let galleryReview = null;
         if (this.state.galleryReviewOn) {
             let galleryReviewSettings = {
                 onClickCloseReview: this.handlerClickCloseReview,
                 projects: this.props.projects,
                 initialSlide: this.state.galleryElementIndex
             };
-            GalleryReview = <GalleryReview {...galleryReviewSettings} />
+            galleryReview = <GalleryReview {...galleryReviewSettings} />
         }
 
         return (
@@ -81,7 +81,7 @@ class Portfolio extends React.Component {
                     <Waypoint onEnter={this.props.onChangeSection.bind(this, 'portfolio')} />
                 </div>
                 <div className={`mask ${(this.state.galleryReviewOn) ? 'mask--gallery-review' : ''}`}>
-                    {GalleryReview}
+                    {galleryReview}
                 </div>
 
                 <GuidLines
