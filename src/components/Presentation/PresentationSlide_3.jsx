93323534c90ffada5e4a7aacd287debe9bc871e9
Sergey.Illarionov <easthunch@gmail.com>
2016-08-07 15:21:58 +0300

    add: redesign slide 3

--- src/components/Presentation/PresentationSlide_3.jsx
+++ src/components/Presentation/PresentationSlide_3.jsx
@@ -6,19 +6,21 @@ class Slide_3 extends React.Component {
 
     render() {
         return(
-            <div
-                className="presentation__slide presentation__slide--3"
-            >
-                <div className="presentation__body">
+            <div className="presentation__slide presentation__slide--3">
+            <div className="presentation__body">
+                <div className="presentation__wrapper">
+                    <div className="presentation__slide-number">{this.props.slideNumber}</div>
                     <div className="presentation__intro">
                         <h2 className="presentation__lead-in">
-                            mobile<br /> application<br /> design.
+                            Mobile<br/> application<br/> design
                         </h2>
                         <div className="presentation__text">
-                            from ideas to mockups<br /> to design<br /> to your enjoy
+                            From sketch, proceeding to mockups <br/>
+                            proceeding to design
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
         )
     }
