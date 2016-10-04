ec0eaf9c80fdeb3e76d3b7f7e1ba7fcbd3864465
Sergey.Illarionov <easthunch@gmail.com>
2016-10-04 18:29:36 +0300

    add: menu popup to navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,68 +36,68 @@ class Footer extends React.Component {
                                 imagination <br/> is the <span className="lead-header__underline">only</span> limit.
                             </h2>
                             <div className="page__content">
-                                <div className="branches branches--footer">
-                                    <div className="branches__item">
-                                        <div className="branches__head branches__head--marked">About us</div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">our team</a></div>
-                                            <div className="branches__point-description">our story in faces</div>
+                                <div className="menu menu--footer">
+                                    <div className="menu__item">
+                                        <div className="menu__head menu__head--marked">About us</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">our team</a></div>
+                                            <div className="menu__point-description">our story in faces</div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">our vision</a></div>
-                                            <div className="branches__point-description">why/how we are doing design</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">our vision</a></div>
+                                            <div className="menu__point-description">why/how we are doing design</div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">our process</a></div>
-                                            <div className="branches__point-description">how the magic happens</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">our process</a></div>
+                                            <div className="menu__point-description">how the magic happens</div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">blog</a></div>
-                                        </div>
-                                    </div>
-                                    <div className="branches__item">
-                                        <div className="branches__head"><a href="#">Our work</a></div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">web sites</a></div>
-                                        </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">branding</a></div>
-                                            <div className="branches__point-description">brand is not just a logo</div>
-                                        </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">mobile apps</a></div>
-                                            <div className="branches__point-description">ios / android</div>
-                                        </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">graphic design</a></div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">blog</a></div>
                                         </div>
                                     </div>
-                                    <div className="branches__item">
-                                        <div className="branches__head branches__head--branding">Agency</div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">download our presentation</a></div>
+                                    <div className="menu__item">
+                                        <div className="menu__head"><a href="#">Our work</a></div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">web sites</a></div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-description">or<br/></div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">branding</a></div>
+                                            <div className="menu__point-description">brand is not just a logo</div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name"><a href="#">fill the brief</a></div>
-                                            <div className="branches__point-description">let’s make something awesome together</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">mobile apps</a></div>
+                                            <div className="menu__point-description">ios / android</div>
+                                        </div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">graphic design</a></div>
                                         </div>
                                     </div>
-                                    <div className="branches__item">
-                                        <div className="branches__head">Contacts</div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name">email</div>
-                                            <div className="branches__point-description"><a href="mailto:sales@whaledesign.me">sales@whaledesign.me</a></div>
+                                    <div className="menu__item">
+                                        <div className="menu__head menu__head--branding">Agency</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">download our presentation</a></div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name">skype</div>
-                                            <div className="branches__point-description"><a href="skype:whaledesign?call">whaledesign.me</a></div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-description">or<br/></div>
                                         </div>
-                                        <div className="branches__point">
-                                            <div className="branches__point-name">phone</div>
-                                            <div className="branches__point-description">+38 (097) 789 08 00</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name"><a href="#">fill the brief</a></div>
+                                            <div className="menu__point-description">let’s make something awesome together</div>
+                                        </div>
+                                    </div>
+                                    <div className="menu__item">
+                                        <div className="menu__head">Contacts</div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name">email</div>
+                                            <div className="menu__point-description"><a href="mailto:sales@whaledesign.me">sales@whaledesign.me</a></div>
+                                        </div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name">skype</div>
+                                            <div className="menu__point-description"><a href="skype:whaledesign?call">whaledesign.me</a></div>
+                                        </div>
+                                        <div className="menu__point">
+                                            <div className="menu__point-name">phone</div>
+                                            <div className="menu__point-description">+38 (097) 789 08 00</div>
                                         </div>
                                     </div>
                                 </div>
@@ -126,68 +126,6 @@ class Footer extends React.Component {
                 <div className="footer__since">
                     <span>since</span>
                     <span>2016</span>
-                </div>
-                <div className={`popup popup--footer ${(this.state.isPopupActive) ? 'popup--show': ''}`}>
-                    <span
-                        className="close-btn close-btn--footer"
-                        onClick={this.handlerClosePopup}
-                    ></span>
-                    <div className="popup__wrapper">
-                        <div className="lead-header lead-header--footer">
-                            imagination <br/> is the <span className="lead-header__underline">only</span> limit.
-                        </div>
-                        <div className="branches branches--footer">
-                            <div className="branches__item">
-                                <div className="branches__head branches__head--marked branches__head--marked-white">About us</div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">our team</a></div>
-                                    <div className="branches__point-description">our story in faces</div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">our vision</a></div>
-                                    <div className="branches__point-description">why/how we are doing design</div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">our process</a></div>
-                                    <div className="branches__point-description">how the magic happens</div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">blog</a></div>
-                                </div>
-                            </div>
-                            <div className="branches__item">
-                                <div className="branches__head"><a href="#">Our work</a></div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">web sites</a></div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">branding</a></div>
-                                    <div className="branches__point-description">brand is not just a logo</div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">mobile apps</a></div>
-                                    <div className="branches__point-description">ios / android</div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">graphic design</a></div>
-                                </div>
-                            </div>
-                            <div className="branches__item">
-                                <div className="branches__head branches__head--branding">Agency</div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">download our presentation</a></div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-description">or<br/></div>
-                                </div>
-                                <div className="branches__point">
-                                    <div className="branches__point-name"><a href="#">fill the brief</a></div>
-                                    <div className="branches__point-description">let’s make something awesome together</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="footer__menu-sign">*M ENU</div>
-                    </div>
                 </div>
 
                 <GuidLines/>
