2d2da8f77b61aa6bf0c9b8bb2c1250dd130f9f6e
Sergey.Illarionov <easthunch@gmail.com>
2016-10-04 20:36:03 +0300

    refactoring: navigation popup

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,27 +2,9 @@ import React from "react";
 import classNames from 'classnames';
 import './Navigation.less';
 
-import Menu from './Menu.jsx';
-
 class Navigation extends React.Component{
     static defaultProps = {
         spanAmount: [1, 2, 3, 4]
-    }
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isNavigationActive: false
-        }
-    }
-
-    handlerShowNavigation = (e) => {
-        const isHasOpenClassName = e.currentTarget.classList.contains('navigation__icon--open');
-
-        this.setState({
-            isNavigationActive: !isHasOpenClassName
-        })
     }
 
     render() {
@@ -30,28 +12,18 @@ class Navigation extends React.Component{
         const navIconClassNames = classNames('navigation__icon', {
             'navigation__icon--white': navIconColorModifier
         }, {
-            'navigation__icon--open navigation__icon--white': this.state.isNavigationActive
+            'navigation__icon--open': this.props.isNavigationMenuActive
         })
 
         return (
             <nav className="navigation">
                 <div
                     className={navIconClassNames}
-                    onClick={this.handlerShowNavigation}
+                    onClick={this.props.handlerShowMenuNavigation}
                 >
                     { this.props.spanAmount.map((num, index) => {
                         return <span key={index} />
                     })}
-                </div>
-
-                <div className={`popup popup--navigation ${(this.state.isNavigationActive) ? 'popup--show': ''}`}>
-                    <div className="popup__wrapper">
-                        <div className="lead-header lead-header--footer">
-                            imagination <br/> is the <span className="lead-header__underline">only</span> limit.
-                        </div>
-                        <Menu className="branches branches--navigation" />
-                        <div className="navigation__sign">*M ENU</div>
-                    </div>
                 </div>
             </nav>
         )
