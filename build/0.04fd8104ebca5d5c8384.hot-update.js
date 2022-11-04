c504370d289d4c48decb30b223f32ccb3b67d467
Serhii Illarionov <sergio.illarionov@gmail.com>
2022-11-04 11:44:54 +0200

    Updates

---
+++ build/0.04fd8104ebca5d5c8384.hot-update.js
@@ -0,0 +1,140 @@
+webpackHotUpdate(0,{
+
+/***/ 667:
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(81);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	// import Parallax from '../../utils/ParallaxBg.jsx';
+
+
+	var Slide_1 = function (_React$Component) {
+	    _inherits(Slide_1, _React$Component);
+
+	    function Slide_1() {
+	        _classCallCheck(this, Slide_1);
+
+	        return _possibleConstructorReturn(this, (Slide_1.__proto__ || Object.getPrototypeOf(Slide_1)).apply(this, arguments));
+	    }
+
+	    _createClass(Slide_1, [{
+	        key: "render",
+	        value: function render() {
+	            return _react2.default.createElement(
+	                "div",
+	                { className: "presentation__slide presentation__slide--1" },
+	                _react2.default.createElement(
+	                    "div",
+	                    { className: "presentation__body" },
+	                    _react2.default.createElement(
+	                        "div",
+	                        { className: "presentation__intro" },
+	                        _react2.default.createElement(
+	                            "h2",
+	                            { className: "lead-header lead-header--presentation" },
+	                            "imagination",
+	                            _react2.default.createElement("br", null),
+	                            " is the ",
+	                            _react2.default.createElement(
+	                                "span",
+	                                { className: "lead-header__underline" },
+	                                "only"
+	                            ),
+	                            " limit."
+	                        ),
+	                        _react2.default.createElement(
+	                            "div",
+	                            { className: "presentation__text" },
+	                            "we are keeping our hand on pulse of trends",
+	                            _react2.default.createElement("br", null),
+	                            "but still always one step ahead"
+	                        )
+	                    ),
+	                    _react2.default.createElement(
+	                        "h3",
+	                        { className: "brand-name brand-name--presentation" },
+	                        _react2.default.createElement(
+	                            "div",
+	                            {
+	                                // parallaxSpeed={.15}
+	                                className: "brand-name__text"
+	                            },
+	                            _react2.default.createElement(
+	                                "sup",
+	                                { className: "brand-name__star" },
+	                                "*"
+	                            ),
+	                            _react2.default.createElement(
+	                                "i",
+	                                { className: "brand-name__W" },
+	                                "w"
+	                            ),
+	                            _react2.default.createElement(
+	                                "i",
+	                                { className: "brand-name__H" },
+	                                "h"
+	                            ),
+	                            _react2.default.createElement("br", null),
+	                            _react2.default.createElement(
+	                                "i",
+	                                { className: "brand-name__A" },
+	                                "a"
+	                            ),
+	                            _react2.default.createElement(
+	                                "i",
+	                                { className: "brand-name__L" },
+	                                "l"
+	                            ),
+	                            _react2.default.createElement("br", null),
+	                            _react2.default.createElement(
+	                                "i",
+	                                { className: "brand-name__E" },
+	                                "e"
+	                            )
+	                        )
+	                    )
+	                )
+	            );
+	        }
+	    }]);
+
+	    return Slide_1;
+	}(_react2.default.Component);
+
+	var _default = Slide_1;
+	exports.default = _default;
+	;
+
+	(function () {
+	    if (typeof __REACT_HOT_LOADER__ === 'undefined') {
+	        return;
+	    }
+
+	    __REACT_HOT_LOADER__.register(Slide_1, "Slide_1", "/Users/maplemap/Work/Sources/_playground/whale-creative-agency-page/src/components/Presentation/PresentationSlide_1.jsx");
+
+	    __REACT_HOT_LOADER__.register(_default, "default", "/Users/maplemap/Work/Sources/_playground/whale-creative-agency-page/src/components/Presentation/PresentationSlide_1.jsx");
+	})();
+
+	;
+
+/***/ })
+
+})
